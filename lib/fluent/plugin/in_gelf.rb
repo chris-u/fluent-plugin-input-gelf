8a0e1b21b56c92ce7d2358b780ec6b72d70ea5d7
Chris Ulrich <culrich@athenahealth.com>
2019-12-23 10:55:23 -0500

    minor refactor of time management and comments

--- lib/fluent/plugin/in_gelf.rb
+++ lib/fluent/plugin/in_gelf.rb
@@ -76,14 +76,14 @@ module Fluent::Plugin
         end
 
         if @trust_client_timestamp && record.key?('timestamp')
-          # Use the recorded event time if available
+          seconds = record['timestamp'].to_i
+          # Fluent "time" is made up of 2 records; time_t and nsec; you can't cast a float
+          # to time; instead you must convert the remainder to a nsec INT.
+
           if @client_timestamp_to_i
-            time = record['timestamp'].to_i
-          else
-            seconds = record['timestamp'].to_i
             nsec = ((record['timestamp'].to_f  - record['timestamp'].to_i)  * 1_000_000_000).to_i
-            time = Fluent::EventTime.new(seconds, nsec)
           end
+          time = Fluent::EventTime.new(seconds, nsec)
           record.delete('timestamp') if @remove_timestamp_record
         else
           time = Fluent::EventTime.now
